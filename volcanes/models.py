8553b3dd8f57fbd64f705d4528ccdfb531707145
geekecuador <dapl@me.com>
2014-06-29 13:31:46 -0500

    Ok
    Ok

--- volcanes/models.py
+++ volcanes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Volcan(models.Model):
-	nombre = models.CharField(max_length=25)
+	nombre = models.CharField(max_length=26)
 	altura = models.PositiveIntegerField()
 	imagen = models.Fileield(upload_to='cargas/')
 	descripcion = models.TextField()
